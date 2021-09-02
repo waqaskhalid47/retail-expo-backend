792b0c9523eef3b8d60453e84350480d28721d18
waqaskhalid48 <waqas.khalid@basecampdata.com>
2021-09-02 20:19:19 +0100

    initial commit for retail expo backend by waqas

---
+++ README.md
@@ -0,0 +1,16 @@
+# final-app
+
+Admin credentials:
+Username: admin@gmail.com
+Password: admin123
+
+Heroku link:  https://final-expo.herokuapp.com/
+
+
+To run on localhost, you need to change url in following directories with your localhost
+
+1. Services/GenericServices.js
+2. components/actions/addtocart.js and productList.js and productDetails.js
+3. components/checkout.js
+
+
---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require("http-errors");
+var express = require("express");
+// var favicon = require("serve-favicon");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/api/users");
+var productsRouter = require("./routes/api/products");
+var config = require("config");
+var app = express();
+var cors = require("cors");
+
+mongoose
+  .connect(config.get("db"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo...."))
+  .catch((error) => console.log(error.message));
+module.exports = app;
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(cookieParser());
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/products", productsRouter);
+app.use("/api/users", usersRouter);
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
